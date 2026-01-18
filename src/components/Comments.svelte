fbd7f31b86f30981eef27240b33656eeb18b8076
The Daniel <dmnyc@users.noreply.github.com>
2026-01-17 23:16:35 -0500

    Hide comment composer when signed out

--- src/components/Comments.svelte
+++ src/components/Comments.svelte
@@ -885,60 +885,65 @@
   <!-- Add Comment Form -->
   <div class="space-y-3 pt-4 border-t">
     <h3 class="text-lg font-semibold">Add a comment</h3>
-    <div class="relative">
-      <div
-        id="comment-input"
-        bind:this={commentComposerEl}
-        class="comment-composer-input w-full px-4 py-3 text-base input rounded-lg"
-        contenteditable="true"
-        role="textbox"
-        aria-multiline="true"
-        data-placeholder="Share your thoughts..."
-        on:input={handleCommentInput}
-        on:keydown={handleCommentKeydown}
-        on:beforeinput={handleBeforeInput}
-        on:paste={handlePaste}
-      ></div>
+    {#if $ndk?.signer}
+      <div class="relative">
+        <div
+          id="comment-input"
+          bind:this={commentComposerEl}
+          class="comment-composer-input w-full px-4 py-3 text-base input rounded-lg"
+          contenteditable="true"
+          role="textbox"
+          aria-multiline="true"
+          data-placeholder="Share your thoughts..."
+          on:input={handleCommentInput}
+          on:keydown={handleCommentKeydown}
+          on:beforeinput={handleBeforeInput}
+          on:paste={handlePaste}
+        ></div>
 
-      {#if showMentionSuggestions}
-        <div class="mention-dropdown" style="border-color: var(--color-input-border);">
-          {#if mentionSuggestions.length > 0}
-            <div class="mention-dropdown-content">
-              {#each mentionSuggestions as suggestion, index}
-                <button
-                  type="button"
-                  on:click={() => insertMention(suggestion)}
-                  on:mousedown|preventDefault={() => insertMention(suggestion)}
-                  class="mention-option"
-                  class:mention-selected={index === selectedMentionIndex}
-                >
-                  <CustomAvatar pubkey={suggestion.pubkey} size={24} />
-                  <div class="mention-info">
-                    <span class="mention-name">{suggestion.name}</span>
-                    {#if suggestion.nip05}
-                      <span class="mention-nip05">{suggestion.nip05}</span>
-                    {/if}
-                  </div>
-                </button>
-              {/each}
-            </div>
-          {:else if mentionSearching}
-            <div class="mention-empty">Searching...</div>
-          {:else if mentionQuery.length > 0}
-            <div class="mention-empty">No users found</div>
-          {/if}
-        </div>
-      {/if}
-    </div>
-    <Button
-      on:click={(e) => {
-        e.preventDefault?.();
-        postComment();
-      }}
-      disabled={!commentText.trim()}
-    >
-      Post Comment
-    </Button>
+        {#if showMentionSuggestions}
+          <div class="mention-dropdown" style="border-color: var(--color-input-border);">
+            {#if mentionSuggestions.length > 0}
+              <div class="mention-dropdown-content">
+                {#each mentionSuggestions as suggestion, index}
+                  <button
+                    type="button"
+                    on:click={() => insertMention(suggestion)}
+                    on:mousedown|preventDefault={() => insertMention(suggestion)}
+                    class="mention-option"
+                    class:mention-selected={index === selectedMentionIndex}
+                  >
+                    <CustomAvatar pubkey={suggestion.pubkey} size={24} />
+                    <div class="mention-info">
+                      <span class="mention-name">{suggestion.name}</span>
+                      {#if suggestion.nip05}
+                        <span class="mention-nip05">{suggestion.nip05}</span>
+                      {/if}
+                    </div>
+                  </button>
+                {/each}
+              </div>
+            {:else if mentionSearching}
+              <div class="mention-empty">Searching...</div>
+            {:else if mentionQuery.length > 0}
+              <div class="mention-empty">No users found</div>
+            {/if}
+          </div>
+        {/if}
+      </div>
+      <Button
+        on:click={(e) => {
+          e.preventDefault?.();
+          postComment();
+        }}
+        disabled={!commentText.trim()}
+      >
+        Post Comment
+      </Button>
+    {:else}
+      <p class="text-sm text-caption">Sign in to comment.</p>
+      <a href="/login" class="text-sm underline hover:opacity-80">Sign in</a>
+    {/if}
   </div>
 </div>
 
